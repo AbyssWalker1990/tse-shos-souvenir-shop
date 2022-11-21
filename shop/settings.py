042cefac8bce888f221f7dc582007bc8cc400e3d
Vova <bordiian.v@gmail.com>
2022-11-21 17:58:10 +0200

    done many preparations for deploying on Heroku

--- shop/settings.py
+++ shop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'souvenir-shop-bord.herokuapp.com']
 
 # Application definition
 
@@ -139,3 +139,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    DEBUG = False
