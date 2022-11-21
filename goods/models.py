278186048ed29809522f812dd5cdb4ad0289c919
Vova <bordiian.v@gmail.com>
2022-11-21 16:58:08 +0200

    added dummy info for testing

--- goods/models.py
+++ goods/models.py
@@ -8,7 +8,7 @@ from userprofile.models import Profile
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    featured_img = models.ImageField(default="default.png")
+    featured_img = models.ImageField(upload_to='category/', default="default.png")
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
@@ -23,10 +23,10 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.FloatField(max_length=30)
     discount = models.FloatField(max_length=30, null=True, blank=True)
-    featured_img = models.ImageField(default="default.png")
-    img1 = models.ImageField(null=True, blank=True, default="default.png")
-    img2 = models.ImageField(null=True, blank=True, default="default.png")
-    img3 = models.ImageField(null=True, blank=True, default="default.png")
+    featured_img = models.ImageField(upload_to='goods/', default="default.png")
+    img1 = models.ImageField(upload_to='goods/', null=True, blank=True, default="default.png")
+    img2 = models.ImageField(upload_to='goods/', blank=True, default="default.png")
+    img3 = models.ImageField(upload_to='goods/',null=True, blank=True, default="default.png")
     tags = models.ManyToManyField('Tag', blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
