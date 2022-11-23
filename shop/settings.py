46ec13f6dbebf06b313b3d3b11ceab90eeca915c
Vova <bordiian.v@gmail.com>
2022-11-23 19:32:30 +0200

    database connection fixed

--- shop/settings.py
+++ shop/settings.py
@@ -108,7 +108,8 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # }
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
