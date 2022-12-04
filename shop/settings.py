9bbbec9c8a2f88a237c6a5773fa490e43ba1aa1d
Vova <bordiian.v@gmail.com>
2022-12-04 21:00:53 +0200

    added allowed hosts for new domen

--- shop/settings.py
+++ shop/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'souvenir-shop-bord.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'souvenir-shop-bord.herokuapp.com', 'www.tse-shos.shop', 'tse-shos.shop']
 
 # Application definition
 
