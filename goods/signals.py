551696f2882c617271538d56c218abff0b5c675f
Vova <bordiian.v@gmail.com>
2022-12-10 23:57:12 +0200

    added auto email to customer about product order, need to test

--- goods/signals.py
+++ goods/signals.py
@@ -14,5 +14,26 @@ def delete_bucket_item(sender, instance, created, **kwargs):
             i.save()
 
 
+def send_order_info(sender, instance, created, **kwargs):
+    if created:
+        order_card = instance
+        order_products = OrderProduct.objects.all().filter(session_id=order_card)
+        print("ORDER: ", order_products)
+        user_mail = order_card.email
+        print(user_mail)
+
+        subject = "Дякуємо за покупку у нашому магазині 'Це Щось!'"
+        message = f"Ваше замовлення: ({order_products}) прийнято. Незабаром з вами зв'яжуться для уточнення деталей. " \
+                  f"Гарного дня!"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [user_mail],
+            fail_silently=False,
+        )
+
 
 post_save.connect(delete_bucket_item, sender=OrderCard)
+post_save.connect(send_order_info, sender=OrderCard)
